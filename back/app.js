a14d6cf89cc6bf688ce7b7fd6b413edd1e469333
brian-h2 <brianheredia200309@gmail.com>
2024-09-10 15:52:36 -0300

    Fixeando get

--- back/app.js
+++ back/app.js
@@ -1,14 +1,40 @@
-import express from 'express';
+import express, {json} from 'express';
 import {readJSON } from './utils/lecturaJSON.js'
+import cors from 'cors';
 
 const users =  readJSON('../user.json');
 
 const app = express()
 const port = process.env.PORT ?? 3000
+app.use(json())
+app.use(cors({
+    origin: (origin,callback) => {
+        const acceptedOrigin = [ //Lista de dominios permitidos
+            "http://localhost:8080",
+            "http://localhost:1234",
+            "http://movies.com"
+        ]
+        if(acceptedOrigin.includes(origin)) {
+            return callback(null,true)
+        }
 
+        if(!origin) {
+            return callback(null,true)
+        }
+
+        return callback(new Error('Not allowed by CORS'))
+    }
+}))
 
 app.get('/login',(req,res) => {
     res.send(users).json
+})
+
+app.get('/login:email',(req,res) =>  {
+    const email = req.params
+    const usuarios = users.find(user => user.email === email)
+    if(usuarios) return res.json(usuarios)
+    
 })
 
 app.listen(port, () => {
