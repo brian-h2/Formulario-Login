607ae24a706b5432e3576dec8fb0525b1fe8152d
brian-h2 <brianheredia200309@gmail.com>
2024-09-11 11:04:58 -0300

    Agregado de inicio de sesion con get, posterior lo hare con post para mayor seguridad

--- back/app.js
+++ back/app.js
@@ -2,11 +2,12 @@ import express, {json} from 'express';
 import {readJSON } from './utils/lecturaJSON.js'
 import cors from 'cors';
 
+const app = express()
+app.use(json())
+const port = process.env.PORT ?? 3000
+
 const users =  readJSON('../user.json');
 
-const app = express()
-const port = process.env.PORT ?? 3000
-app.use(json())
 app.use(cors({
     origin: (origin,callback) => {
         const acceptedOrigin = [ //Lista de dominios permitidos
@@ -30,12 +31,19 @@ app.get('/login',(req,res) => {
     res.send(users).json
 })
 
-app.get('/login:email',(req,res) =>  {
-    const email = req.params
-    const usuarios = users.find(user => user.email === email)
-    if(usuarios) return res.json(usuarios)
-    
+app.get('/login/:email.:password',(req,res) =>  { 
+    const email = req.params.email;
+    const password = req.params.password;
+    const usuarios = users.find((element) => element.email === email && element.password.toString() === password)
+    if (usuarios) {
+        // Si el usuario se encuentra, devuelve el HTML y la información del usuario
+        res.send(`<p>Usuario encontrado:</p> <p>Email: ${usuarios.email}</p><p>Contraseña:  ${usuarios.password}`);
+    } else {
+        // Si no se encuentra el usuario, devuelve un mensaje de error
+        res.status(404).send('Usuario no encontrado');
+    }
 })
+
 
 app.listen(port, () => {
     console.log('Server is listening in ',port)
