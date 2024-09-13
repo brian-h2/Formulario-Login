bfd16d935e6f6a8848803ac00aad75fc6da96051
brian-h2 <brianheredia200309@gmail.com>
2024-09-13 10:22:11 -0300

    Implementacion de router para hacer refactoring y modularizar los metodos

--- back/app.js
+++ back/app.js
@@ -1,13 +1,11 @@
-import express, {json} from 'express';
-import {readJSON } from './utils/lecturaJSON.js'
-import { validateUser } from './schemas/schem.js';
 import cors from 'cors';
+import { usersRouter } from './routes/users.js';
+import express, {json} from 'express';
 
 const app = express()
+app.disable('x-powered-by');
 app.use(json())
 const port = process.env.PORT ?? 3000
-
-const users =  readJSON('../user.json');
 
 app.use(cors({
     origin: (origin,callback) => {
@@ -28,69 +26,7 @@ app.use(cors({
     }
 }))
 
-app.get('/login',(req,res) => {
-    res.send(users).json
-})
-
-app.get('/login',(req,res) =>  { 
-    const {email,password} = req.body
-    if(email != "" && password != "") {
-        const usuarios = users.find((element) => element.email === email && element.password.toString() === password)
-        if (usuarios) {
-            // Si el usuario se encuentra, devuelve el HTML y la información del usuario
-           return res.sendStatus(201)
-        } else {
-            // Si no se encuentra el usuario, devuelve un mensaje de error
-            return res.status(404).send('Usuario no encontrado');
-        } 
-    } 
-})
-
-app.post('/login',(req,res) =>  {
-    const {email,password} = req.body;
-    
-    const usuarioExistente = users.find(user => user.email === email)
-
-    if(usuarioExistente) {
-        return res.status(400).send('El email ya esta registrado')
-    }
-
-    const newUser = {
-        id: users.length + 1,
-        email: email,
-        password: password
-    }
-
-    users.push(newUser)
-
-    res.status(201).send('Usuario registrado correctamente')
-})
-
-
-app.patch('/login/:id',(req,res) => {
-    const result = validateUser(req.body)
-    console.log(result)
-
-    if(!result.success) {
-        res.status(400).json({ error: JSON.parse(result.error.message) })
-    } 
-
-    const {id} = req.params
-    const userIndex = users.findIndex(user => user.id.toString() === id)
-    if (userIndex === -1) {
-        return res.status(404).json({ message: 'Usuario no encontrado' })
-    }
-
-    const updateUser = {
-        ...users[userIndex],
-        ...result.data
-    }
-
-    users[userIndex] = updateUser;
-
-    return res.json(updateUser)
-})
-
+app.use('/login',usersRouter)
 
 app.listen(port, () => {
     console.log('Server is listening in ',port)
---
+++ back/routes/users.js
@@ -0,0 +1,73 @@
+import { Router  } from "express";
+import { readJSON } from "../utils/lecturaJSON.js";
+import { validateUser } from "../schemas/schem.js";
+
+const users =  readJSON('../user.json');
+
+export const usersRouter = Router();
+
+usersRouter.get('/login',(req,res) => {
+    res.send(users).json
+})
+
+usersRouter.get('/login',(req,res) =>  { 
+    const {email,password} = req.body
+    if(email != "" && password != "") {
+        const usuarios = users.find((element) => element.email === email && element.password.toString() === password)
+        if (usuarios) {
+            // Si el usuario se encuentra, devuelve el HTML y la información del usuario
+           return res.sendStatus(201)
+        } else {
+            // Si no se encuentra el usuario, devuelve un mensaje de error
+            return res.status(404).send('Usuario no encontrado');
+        } 
+    } 
+})
+
+usersRouter.post('/login',(req,res) =>  {
+    const {email,password} = req.body;
+    
+    const usuarioExistente = users.find(user => user.email === email)
+
+    if(usuarioExistente) {
+        return res.status(400).send('El email ya esta registrado')
+    }
+
+    const newUser = {
+        id: users.length + 1,
+        email: email,
+        password: password
+    }
+
+    users.push(newUser)
+
+    res.status(201).send('Usuario registrado correctamente')
+})
+
+
+
+usersRouter.patch('/login/:id',(req,res) => {
+
+    const result = validateUser(req.body)
+    
+    console.log(result)
+
+    if(!result.success) {
+        res.status(400).json({ error: JSON.parse(result.error.message) })
+    } 
+
+    const {id} = req.params
+    const userIndex = users.findIndex(user => user.id.toString() === id)
+    if (userIndex === -1) {
+        return res.status(404).json({ message: 'Usuario no encontrado' })
+    }
+
+    const updateUser = {
+        ...users[userIndex],
+        ...result.data
+    }
+
+    users[userIndex] = updateUser;
+
+    return res.json(updateUser)
+})
